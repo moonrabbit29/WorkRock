748eff6cb7d2a166dc0740ac32445366856595b7
Elki <elnino.kinan@gmail.com>
2021-05-25 20:51:58 +0700

    add class outsideTemp

--- fuzzy.py
+++ fuzzy.py
@@ -58,8 +58,35 @@ class outsideTemp:
    normalMember = 0
    warmMember = 0
 
+   @classmethod
+   def mildMemberSetter(cls,temperature):
+      if(temperature>15 and temperature<=20):
+         return (temperature-15)/(20-15)
+      elif(temperature>10 and temperature<20):
+         return (25-temperature)/(25-20)
    
-   
+   @classmethod
+   def normalMemberSetter(cls,temperature):
+      if(temperature>20 and temperature<=25):
+         return (temperature-20)/(25-20)
+      elif(temperature>25 and temperature<30):
+         return (30-temperature)/(30-25)
+
+   @classmethod
+   def warmMemberSetter(cls,temperature):
+      if(temperature>25 and temperature<=30):
+         return (temperature-20)/(25-20)
+      elif(temperature>30 and temperature<35):
+         return (35-temperature)/(35-30)
+
+   @classmethod
+   def roomTemperatureMember(cls,temperature):
+      cls.mildMember = (0 if(temperature<=15 or temperature>=25) else 
+                        cls.mildMemberSetter(temperature))
+      cls.normalMember = (0 if(temperature<=20 or temperature>=30) else
+                         cls.normalMemberSetter(temperature))
+      cls.warmMember = (0 if(temperature<=25 or temperature>=35) else
+                         cls.normalMemberSetter(temperature))
 
 
 class Fuzzy :
